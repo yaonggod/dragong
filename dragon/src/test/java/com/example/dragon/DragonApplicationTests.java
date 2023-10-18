f6fb10183c423bdf11783020649c2dd2af417d21
yaonggod <jy23698@naver.com>
2023-10-18 17:34:55 +0900

    init

---
+++ dragon/src/test/java/com/example/dragon/DragonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ dragong/android/app/src/main/kotlin/com/example/dragong/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dragong
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
