46faf1b13764803ed97c17d23ac87b3494df71ef
yaonggod <jy23698@naver.com>
2023-10-26 18:27:17 +0900

    1026

--- dragong_front/lib/login_screen.dart
+++ dragong_front/lib/login_screen.dart
@@ -10,19 +10,23 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool isLogined = false;
-  String? nickname;
 
   Future<bool> naverLogin() async {
-    final NaverLoginResult result = await FlutterNaverLogin.logIn();
-    NaverAccessToken token = await FlutterNaverLogin.currentAccessToken;
-    print(token.accessToken);
-    print(token.refreshToken);
-    print(result.account.email);
-    setState(() {
-      nickname = result.account.nickname;
-      isLogined = true;
-    });
-    return true;
+    try {
+      final NaverLoginResult result = await FlutterNaverLogin.logIn();
+
+      NaverAccessToken token = await FlutterNaverLogin.currentAccessToken;
+      // Naver 인증 서버에 접근하는 AT를 백엔드에 보내기
+      print(token.accessToken);
+      // 만약 백엔드에서 AT로 인증하는 과정을 간소화하고 싶으면 이메일만 보내서 토큰 발급하기
+      // print(result.account.email);
+      setState(() {
+        isLogined = true;
+      });
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 
   @override
@@ -31,7 +35,6 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(title: const Text('네이버 로그인')),
       body: Column(
         children: [
-          nickname != null ? Text(nickname!) : const Text('정보 없음'),
           isLogined ? const Text('로그인됨') : const Text('로그인안됨'),
           ElevatedButton(onPressed: naverLogin, child: const Text('네이버 로그인'))
         ],
